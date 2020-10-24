152e7fd684c4f00487de66d8e35c705fa79f8350
Bong Nguyen <van.bong@grabtaxi.com>
2020-10-24 18:19:15 +0800

    test: add shutdown test

--- application_test.go
+++ application_test.go
@@ -2,9 +2,11 @@ package webkit
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -24,4 +26,31 @@ func Test_GET(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/mock-endpoint", nil)
 	resp := executeRequest(app, req)
 	require.Equal(t, http.StatusOK, resp.Code)
+}
+
+func Test_shutdown(t *testing.T) {
+	app := New()
+	app.srv = &http.Server{
+		Handler: app.router,
+		Addr:    fmt.Sprint(":", app.port),
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	require.NotPanics(t, func() {
+		app.shutdown()
+	})
+
+	select {
+	case _, ok := <-app.shutdownCh:
+		require.False(t, ok, "shutdownCh should be closed")
+	default:
+		require.Fail(t, "shutdownCh should be closed")
+	}
+
+	require.NotPanics(t, func() {
+		app.shutdown()
+	})
+
 }
