e07330483cd2b0495fc1d206e5a3cc11ef2ee754
Bong Nguyen <van.bong@grabtaxi.com>
2020-10-24 17:33:24 +0800

    feat: add graceful shutdown

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # go-webkit
-A webkit for Go with simple APIs to use.
+A webkit for Go with simple APIs to use. It solves common problems for a web server so engineers can focus on business logic.
+
+## Features
+
+- Graceful shutdown
--- application.go
+++ application.go
@@ -1,9 +1,13 @@
 package webkit
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -12,15 +16,18 @@ import (
 // New creates a new application.
 func New() *Application {
 	return &Application{
-		router: mux.NewRouter(),
-		port:   8080,
+		router:     mux.NewRouter(),
+		port:       8080,
+		shutdownCh: make(chan struct{}),
 	}
 }
 
 // Application is a web application.
 type Application struct {
-	router *mux.Router
-	port   int
+	router     *mux.Router
+	port       int
+	wg         sync.WaitGroup
+	shutdownCh chan struct{}
 }
 
 // Run starts an HTTP server.
@@ -33,8 +40,30 @@ func (app *Application) Run() error {
 		ReadTimeout:  15 * time.Second,
 	}
 
-	log.Println("Serving at port", app.port)
-	log.Fatal(srv.ListenAndServe())
+	app.execute(func() {
+		log.Println("Serving at port", app.port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println("Error:", err)
+			close(app.shutdownCh)
+		}
+	})
+
+	app.execute(func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		select {
+		case <-sigint:
+		case <-app.shutdownCh:
+		}
+
+		// We received an interrupt signal, shut down.
+		if err := srv.Shutdown(context.Background()); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+	})
+
+	app.wg.Wait()
 	return nil
 }
 
@@ -42,4 +71,14 @@ func (app *Application) Run() error {
 func (app *Application) GET(path string, h Handler) {
 	route := app.router.Path(path).Methods(http.MethodGet)
 	route.HandlerFunc(buildHandlerFunc(h))
+}
+
+// execute starts a function in a goroutine.
+// It tracks the execution in a WaitGroup for graceful shutdown.
+func (app *Application) execute(fn func()) {
+	app.wg.Add(1)
+	go func() {
+		defer app.wg.Done()
+		fn()
+	}()
 }
