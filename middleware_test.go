ee5fc471b5f00e21e234fd1e58fcfa5183f61923
Bong Nguyen <bongnv@users.noreply.github.com>
2020-10-26 10:27:38 +0800

    feat: add Recovery() option (#10)

---
+++ middleware_test.go
@@ -0,0 +1,23 @@
+package webkit
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var mockMiddleware Middleware = func(h Handler) Handler {
+	return h
+}
+
+func Test_ApplyRoute(t *testing.T) {
+	r := &route{}
+	mockMiddleware.ApplyRoute(r)
+	require.Len(t, r.middlewares, 1)
+}
+
+func Test_Apply(t *testing.T) {
+	a := &Application{}
+	mockMiddleware.Apply(a)
+	require.Len(t, a.routeOptions, 1)
+}
